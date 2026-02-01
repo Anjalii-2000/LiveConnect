3916184ad0cea00e0e293ffb495a5e27b695795c
Anjali Bhardwaj <your-email@gmail.com>
2026-02-02 00:15:46 +0530

    backend url updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001"); // backend URL
+const socket = io("https://liveconnect-server.onrender.com");
+
+// enable below line for local debugging
+//const socket = io("http://localhost:5000"); // backend URL
 
 export default socket;
