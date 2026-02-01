0c65cdc76b151ed7df4eb4f4d0fb2366c459cfad
Anjali Bhardwaj <your-email@gmail.com>
2026-02-01 23:57:56 +0530

    backend deploy support

--- server/server.js
+++ server/server.js
@@ -8,9 +8,17 @@ app.use(cors());
 
 const server = http.createServer(app);
 
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+
 const io = new Server(server, {
   cors: {
     origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 
