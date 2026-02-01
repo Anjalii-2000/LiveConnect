1ec4436af09c5661a4d629b44b896d822dbbf2ce
Anjali Bhardwaj <your-email@gmail.com>
2026-02-02 00:31:35 +0530

    added favicon.png

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://liveconnect-server.onrender.com");
+const socket = io("https://liveconnect-0327.onrender.com");
 
 // enable below line for local debugging
 //const socket = io("http://localhost:5000"); // backend URL
